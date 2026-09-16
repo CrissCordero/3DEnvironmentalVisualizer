fd613a6d8bbdb64806cb2cd5e815069ab02595c9
Phong Nguyen <pnguyen7@umassd.edu>
2026-09-16 13:26:38 -0400

    Fixed bubble sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -3,14 +3,16 @@
 public class BubbleSort {
 
     public static void main(String[] args) {
+        
             
         int arr[] = { 6, 4, 1, 10, 2, 5, 3, 8, 7, 9};
 
-        for (int i = 0; i < (arr.length-1);  i++) {
+        
+        for (int i = 0; i < arr.length-1; i++) {
 
             for (int j = 0; j < arr.length-1-i; j++) {
                 
-                if (arr[j] < arr[j+1]) {
+                if (arr[j] > arr[j+1]) {
 
                     int temp = arr[j];
                     arr[j] = arr[j+1];
@@ -20,9 +22,7 @@ public class BubbleSort {
         }
 
         for (int i = 0; i < arr.length; i++) {
-
             System.out.println(arr[i]);
+            }
         }
     }
-}
-
