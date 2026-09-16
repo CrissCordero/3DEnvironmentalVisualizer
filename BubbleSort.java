09ae551ea0a584ffe6db32b2d578ecf14033cf60
Ronan Maloney <ronanmaloney0@gmail.com>
2026-09-16 13:26:58 -0400

    Bubble Sort

---
+++ BubbleSort.java
@@ -0,0 +1,28 @@
+//A simple algorithm for bubble sorting an array.
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+            
+        int arr[] = { 6, 4, 1, 10, 2, 5, 3, 8, 7, 9};
+
+        for (int i = 0; i < (arr.length-1);  i++) {
+
+            for (int j = 0; j < arr.length-1-i; j++) {
+                
+                if (arr[j] < arr[j+1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+
+            System.out.println(arr[i]);
+        }
+    }
+}
+
